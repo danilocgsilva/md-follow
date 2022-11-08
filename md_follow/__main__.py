dfe75ec70557fe37c3e4519adf82c9364cbff1fb
Danilo Silva <contato@danilocgsilva.me>
2022-11-08 06:49:02 -0300

    Messages to guide user at file creation

--- md_follow/__main__.py
+++ md_follow/__main__.py
@@ -1,3 +1,25 @@
-def main():
-    print("Hello World! Starts here the md-follow program to change the world!")
+import markdown
+import os
 
+def generateHtml(markdownCandidate):
+    
+    print("The file " + markdownCandidate + " will be converted to a html file.")
+    
+    with open(markdownCandidate, 'r') as file:
+        text = file.read()
+        html = markdown.markdown(text)
+        
+    newFileName = markdownCandidate + ".html"
+    with open(newFileName, 'w') as wfile:
+        wfile.write(html)
+        
+    print('New file generated: ' + newFileName)
+
+def main():
+    
+    markdownCandidate = 'README.md'
+    if not os.path.isfile(markdownCandidate):
+        print('The file ' + markdownCandidate + ' was not found in current directory.')
+    else:
+        generateHtml(markdownCandidate)
+    
