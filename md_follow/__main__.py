ab6ba0a7fb15a4a037d3f4aa360dacfb8d95f295
Danilo Silva <contato@danilocgsilva.me>
2022-11-08 07:46:02 -0300

    Options to create a documentation folder

--- md_follow/__main__.py
+++ md_follow/__main__.py
@@ -1,25 +1,44 @@
 import markdown
 import os
+import argparse
 
-def generateHtml(markdownCandidate):
+def generateHtml(markdownCandidate, documentation_folder):
     
     print("The file " + markdownCandidate + " will be converted to a html file.")
     
     with open(markdownCandidate, 'r') as file:
         text = file.read()
         html = markdown.markdown(text)
+    
+    file_name_base = markdownCandidate + ".html"
+    if documentation_folder:
+        os.makedirs('documentation')
+        newFileName = 'documentation/' + file_name_base
+    else:
+        newFileName = file_name_base
         
-    newFileName = markdownCandidate + ".html"
     with open(newFileName, 'w') as wfile:
         wfile.write(html)
         
     print('New file generated: ' + newFileName)
-
+    
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--documentation-folder',
+        '-d',
+        required=False,
+        action='store_true'
+    )
+    return parser.parse_args()
+    
 def main():
+    
+    arguments = get_arguments()
     
     markdownCandidate = 'README.md'
     if not os.path.isfile(markdownCandidate):
         print('The file ' + markdownCandidate + ' was not found in current directory.')
     else:
-        generateHtml(markdownCandidate)
+        generateHtml(markdownCandidate, arguments.documentation_folder)
     
